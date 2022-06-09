796f668d1b53fab96ba2f310c5b3c29bc29e7ef8
wepd <weped_x@163.com>
2022-06-10 00:29:35 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom"
 
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import Home from "./routes/Home/Home"
+import Navigation from "./routes/Navigate/Navigate"
+import SignIn from "./routes/Login/Login"
 
 const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
+  return <h1>I am the shop page</h1>
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
+      <Route path="/" element={<Navigation />}>
+        {/* 当配置/时，头部导航显示，Home组件作为主页面 */}
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,12 +7,12 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyChTdPdmA9jFxwRjvUZd5arUkhpsiXWuro",
+  authDomain: "my-db-e8e03.firebaseapp.com",
+  projectId: "my-db-e8e03",
+  storageBucket: "my-db-e8e03.appspot.com",
+  messagingSenderId: "63756133934",
+  appId: "1:63756133934:web:2f0b367bd7d30e593121bf"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
